f667b37b8ba131373215655539e549ebe3c33de2
kevinlyndonpastoriza@gmail.com <kevinlyndonpastoriza@gmail.com>
2025-07-30 23:52:46 -0700

    feat: add auditable base class and interfaces

---
+++ src/Domain/Entities/Audtiable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public abstract class Audtiable<T> : IAuditable<T>
+    {
+        [Key]
+        public T Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set;
+    }
+}
---
+++ src/Domain/Entities/IAuditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public interface IAuditable
+    {
+        DateTime CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? LastModifiedDate { get; set; }
+        string LastModifiedBy { get; set; }
+    }
+
+    public interface IAuditable<T> : IAuditable
+    {
+        T Id { get; set; }
+    }
+}
